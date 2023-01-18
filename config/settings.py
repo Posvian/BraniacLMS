842b1bf72a0ca300603c56a5b0d6d5603dd689a2
Posvian <103316529+Posvian@users.noreply.github.com>
2023-01-18 15:53:19 +0100

    Lesson 4 (#3)
    lesson_4 Lesson ready

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
 
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            "BraniacLMS/templates",
+            "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
